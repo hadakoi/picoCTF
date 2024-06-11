b09d52657f1e3087de1e2a499b63489a5c48ec39
cathat23 <aryguy2005@gmail.com>
2024-06-11 14:02:12 +0530

    Learning feature

---
+++ C/Test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    y = 0;
+    for (x = 0; y < 23; x++) {
+        int check;
+        check = x % 2;
+        if (check == 0) {
+            continue; // Skip even numbers
+        }
+        printf("%d ", x); // Print odd number
+        y++; // Increment y only when an odd number is printed
+    }
+    return 0;
+}
